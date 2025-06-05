76d24254a322d217f7bdf7c00e163f5ee20875e8
aposp <aposporisger@gmail.com>
2025-06-05 17:40:39 +0300

    wip

--- src/components/bio/BioTextField.vue
+++ src/components/bio/BioTextField.vue
@@ -1,17 +1,24 @@
 <template>
   <div>
     <ion-textarea v-model="globalStore.user.bio"
-                 fill="solid"
+                 fill="outline"
                  :auto-grow="true"
                  placeholder="Enter text"
     ></ion-textarea>
-
+  <ion-button class="mt-3" label="Αποθήκευση" @click="updateBio">Αποθήκευση</ion-button>
   </div>
 </template>
 
 <script setup>
-import { IonTextarea } from "@ionic/vue";
+import { IonTextarea, IonButton } from "@ionic/vue";
 import {useGlobalStore} from "@/stores/globalStore.js";
 
 const globalStore = useGlobalStore()
+
+async function updateBio(){
+  await axios.post(`user/updateBio`, {
+    bio: globalStore.user.bio
+  })
+
+}
 </script>
